6dd7d6b967bfd304e556315a14a195d8f1618a0d
Runelt <106849784+Runelt@users.noreply.github.com>
2025-10-23 01:28:37 +0900

    Update server.js

--- server.js
+++ server.js
@@ -209,6 +209,32 @@ app.use('/uploads', express.static(uploadDir));
             res.status(500).json({ error: 'DB delete error' });
         }
     });
+
+// URL 정리
+app.get('/:page.html', (req, res, next) => {
+  const page = req.params.page;
+  if (!/^[\w-]+$/.test(page)) return next(); // 경로 조작 방지
+  if (page === 'index') return res.redirect(301, '/');
+  return res.redirect(301, `/${page}`);
+});
+app.get('/index', (req, res) => {
+  res.redirect(301, '/');
+});
+// 파일 찾기
+app.use(
+  express.static(PUBLIC_DIR, {
+    extensions: ['html'],
+    index: 'index.html',
+  })
+);
+// index 반환
+app.get('/', (req, res) => {
+  res.sendFile(path.join(PUBLIC_DIR, 'index.html'));
+});
+// ERROR 404
+app.use((req, res) => {
+  res.status(404).send('404 Not Found');
+});
     
     // 서버 시작
     app.listen(PORT, () => {
